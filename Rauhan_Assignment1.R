7f9702b64b33ed623c72a47c480237a6487c866d
Rauhan Nazir <rauhan.nazir@gmail.com>
2022-01-26 00:15:29 +0100

    Exploratory analysis and further filtering

--- Rauhan_Assignment1.R
+++ Rauhan_Assignment1.R
@@ -33,9 +33,35 @@ elementary_middle_school_teachers <- data[occ2012 == 2310]
 #Looking at the structure of data loaded to get an idea of variables I am working with 
 str(elementary_middle_school_teachers)
 
-#Doing the exploratory analysis on the data table I am working with 
+## Doing the exploratory analysis on the data table I am working with 
 
 skim(elementary_middle_school_teachers)
+summary(elementary_middle_school_teachers)
+
+#Creating a new variable for wage per hour(wph), which will be the target variable 
+
+elementary_middle_school_teachers <-  elementary_middle_school_teachers[, wph := earnwke/uhours]
+
+# Creating data summary for the target variable and some of the important predictors
+datasummary(wph + grade92 + age ~ Mean + SD + Min + Max + P25 + P75 + N , data = elementary_middle_school_teachers)
+
+
+## There is not a lot of variation in the grades for this occupation, which could mean that it might not be able to
+# play a key part in prediction as it does not have a lot of data points. Age on the other hand has more variation.
+
+
+#Filtering the data for possible errors 
+
+#The mean of wage is around $26, so anything less than 10 might be a error and on top of that I only want to consider
+#the full time employees, so I will include a filter for that as well
+
+elementary_middle_school_teachers <- elementary_middle_school_teachers[wph > 10]
+elementary_middle_school_teachers <- elementary_middle_school_teachers[uhours >= 32] #as per the IRS website
+
+#Another important thing that we need to do is to find out the missing values for variables
+
+
+
 
 
 
